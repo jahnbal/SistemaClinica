dd8618bc90c9eb28bd8d3d029408fffd8f9026e9
Fernando Moraes <fernandomoraesfereira074@gmail.com>
2026-07-06 18:32:04 -0300

    Comentario para lembrar da funcao

--- menus/menu_gerenciar_cadastros_produto.c
+++ menus/menu_gerenciar_cadastros_produto.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #include "menu_gerenciar_cadastros_produto.h"
+#include "../structs/produto.c"
 #include "../utils/utilidades.h"
+#include <stdio.h>
 
 /*
 
@@ -17,7 +18,8 @@ obterOpcao() → captura a opção do usuário.
 executarOpcaoMenuPrincipal() → decide qual ação executar.
 
 
-Para cada função MenuX o padrão é esse. MenuPrincipal está de acordo com o padrão 
+Para cada função MenuX o padrão é esse. MenuPrincipal está de acordo com o
+padrão
 {
     char opcao;
 
@@ -33,59 +35,55 @@ Para cada função MenuX o padrão é esse. MenuPrincipal está de acordo com o 
 
     return 0;
 }
-*/ 
+*/
 static void imprimirMenu(void);
 static void executarOpcaoMenu(char opcao);
 
+extern void MenuGerenciarCadastrosProduto(void) {
+  char opcao;
 
-extern void MenuGerenciarCadastrosProduto(void)
-{
-    char opcao;
-
-    do {
-        imprimirMenu();
-        imprimirPrompt();
-        opcao = getOpcao();
-        executarOpcaoMenu(opcao);
-    } while(opcao != '4');
+  do {
+    imprimirMenu();
+    imprimirPrompt();
+    opcao = getOpcao();
+    executarOpcaoMenu(opcao);
+  } while (opcao != '4');
 }
 
-static void imprimirMenu(void)
-{
-    limparTela();
-    printf("\n========================================\n");
-    printf("          MENU PRODUTO\n");
-    printf("========================================\n");
+static void imprimirMenu(void) {
+  limparTela();
+  printf("\n========================================\n");
+  printf("          MENU PRODUTO\n");
+  printf("========================================\n");
 
-    printf("1 - Cadastrar Produto\n");
-    printf("2 - Remover Produto\n");
-    printf("3 - Atualizar Produto\n");
-    printf("4 - Voltar\n");
+  printf("1 - Cadastrar Produto\n");
+  printf("2 - Remover Produto\n");
+  printf("3 - Atualizar Produto\n");
+  printf("4 - Voltar\n");
 }
-static void executarOpcaoMenu(char opcao)
-{
-// APENAS FUNÇÕES NESSE NÍVEL
-    switch(opcao)
-    {
-        case '1':
-            CadastrarProduto();
-            break;
+static void executarOpcaoMenu(char opcao) {
+  // APENAS FUNÇÕES NESSE NÍVEL
+  switch (opcao) {
+  case '1':
+    CadastrarProduto();
+    break;
 
-        case '2':
-            ExcluirProduto();
-            break;
+  case '2':
+    // NDO1: funcao que pega IDs;
+    ExcluirProduto();
+    break;
 
-        case '3':
-            EditarProduto();
-            break;
+  case '3':
+    // NDO1 : Funcao que pega IDs;
+    EditarProduto();
+    break;
 
-        case '4':
-            break;
+  case '4':
+    break;
 
-        default:
-            printf("\nOpcao invalida.");
-            pausar();
-            break;
-    }
+  default:
+    printf("\nOpcao invalida.");
+    pausar();
+    break;
+  }
 }
-
