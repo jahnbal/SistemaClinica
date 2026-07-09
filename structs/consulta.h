2e4e5b80b8cfecf705b393a59e298c2ea44b2fb7
Rodrigo <silveirarthurburner@gmail.com>
2026-07-09 19:55:12 -0300

    comentarios na struct consulta

--- structs/consulta.h
+++ structs/consulta.h
@@ -8,16 +8,16 @@ typedef enum { AGENDADA, EM_ATENDIMENTO, FINALIZADA, CANCELADA } StatusConsulta;
 
 // Estrutura principal de uma consulta
 typedef struct {
-  int id;                  /* identificador único gerado em sequência   */
-  Pet animal;              /* pet em atendimento                        */
-  Veterinario veterinario; /* veterinário responsável                   */
-  float valor;             /* valor cobrado pela consulta               */
-  char data[11];           /* formato DD/MM/AAAA                        */
-  int horario;             /* hora inteira, ex: 9 = 09h00, 14 = 14h00  */
-  StatusConsulta status;   /* estado atual da consulta                  */
+  int id;                  //  Id de cada consulta gerado em sequência
+  Pet animal;              // pet em atendimento
+  Veterinario veterinario; // veterinário responsável
+  float valor;             // valor cobrado pela consulta
+  char data[11];           // formato DD/MM/AAAA
+  int horario;             // hora inteira, ex: 9 = 09h00, 14 = 14h00
+  StatusConsulta status;   // estado atual da consulta
 } Consulta;
 
-// Nó da lista encadeada — LP2: Lista Encadeada
+// Nó da lista encadeada
 typedef struct NoConsulta {
   Consulta dados;
   struct NoConsulta *proximo;
